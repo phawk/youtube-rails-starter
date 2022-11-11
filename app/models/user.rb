30bb443a65f2626c1bf38d40a7035e6b0a8e87f4
Pete Hawkins <petey@hey.com>
2022-11-11 14:27:04 +0000

    Enable devise modules

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  # :confirmable, :timeoutable, and :omniauthable
+  devise :database_authenticatable, :registerable, :trackable, :lockable,
          :recoverable, :rememberable, :validatable
 
   validates :first_name, :last_name, presence: true
