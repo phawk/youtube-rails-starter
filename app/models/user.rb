f0f833e207d0b7eee011aa82d1d5b81049778024
Pete Hawkins <petey@hey.com>
2022-11-20 20:08:19 +0000

    Part 3 styling the app layout using tailwind

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :first_name, :last_name, presence: true
+
+  def full_name
+    [first_name, last_name].join(" ")
+  end
 end
