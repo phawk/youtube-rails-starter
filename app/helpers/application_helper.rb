4e0548e4657ea5bb185db0458c5e13e9316fba8b
Pete Hawkins <petey@hey.com>
2022-11-20 19:45:02 +0000

    Part 2 - styling the auth pages with tailwind UI

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def label_class
+    "block text-sm font-medium text-gray-700"
+  end
+
+  def input_class
+    "block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+  end
+
+  def form_button_class
+    "flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+  end
 end
